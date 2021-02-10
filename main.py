d51bdd2cb4093fae0489a753362f4f55d9903040
MZen2610 <mzen1980@gmail.com>
2021-02-10 19:32:48 +0300

    teстирование pandas

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
+import pandas as pd
+
+# excel_data_df = pd.read_excel('wine.xlsx', sheet_name='Лист1', na_values=['N/A', 'NA'], keep_default_na=False)
 
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
-template = env.get_template('index.html')
+wine_data = []
+# for index, row in excel_data_df.iterrows():
+#     context = {
+#         "name": row['Название'],
+#         "sort": row['Сорт'],
+#         "price": row['Цена'],
+#         "picture": row['Картинка']
+#     }
+#     wine_data.append(context)
 
-rendered_page = template.render(
-    year_title=datetime.datetime.now().year - 1920,
-)
+year = {
+    "year_title": datetime.datetime.now().year - 1920,
+}
+
+template = env.get_template('template.html')
+rendered_page = template.render( year_title=year, wines=wine_data )
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
--- test.py
+++ test.py
@@ -1,3 +1,27 @@
-import datetime
+import pandas as pd
+from pprint import pprint
+import collections
 
-year_title=datetime.datetime.now().year - 1920
+wine2_df = pd.read_excel('wine2.xlsx', sheet_name='Лист1', na_values=['N/A', 'NA'], keep_default_na=False)
+
+wine2_dict = {}
+
+for index, row in wine2_df.iterrows():
+
+    context = {
+       "Название": row['Название'],
+        "Сорт": row['Сорт'],
+        "Цена": row['Цена'],
+        "Картинка": row['Картинка']
+    }
+
+    if row['Категория'] in wine2_dict:
+        wine2_list = []
+        for item in wine2_dict[row['Категория']]:
+            wine2_list.append(item)
+        wine2_list.append(context)
+        wine2_dict[row['Категория']] = wine2_list
+    else:
+        wine2_dict[row['Категория']] = [context]
+
+pprint(wine2_dict)
